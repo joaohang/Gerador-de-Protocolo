42097a6b652f8b8ed309c5d2967cddd8f963daf3
João Hang <j.vitorhang@gmail.com>
2018-04-29 19:44:37 -0300

    Update

--- GeradorDeProtoco.js
+++ GeradorDeProtoco.js
@@ -4,13 +4,12 @@ var respostas = formResponse.getItemResponses();
 
 var emailSolicitante = respostas[0];
 
-var planilha =  SpreadsheetApp.openById('1lyjNBbnlH3vmI5uCcoQJZUhyx_yFfQsXfyOXT7_sqqw');
+var planilha =  SpreadsheetApp.openById('Token from your spreadsheet');
 var sheet = planilha.getActiveSheet();
 
 function myFunction() {
-  // retorna a última linha com dados e escreve texto na coluna 1
   var row = sheet.getLastRow();
-  var soRow = sheet.getLastRow()-1;
-  sheet.getRange(row, 3).setValue(sheet.getRange(soRow, 3).getValue() + 1);  
+  var lastRow = sheet.getLastRow()-1;
+  sheet.getRange(row, 3).setValue(sheet.getRange(lastRow, 3).getValue() + 1);  
   MailApp.sendEmail(emailSolicitante.getResponse(), "Protocolo", "Seu número de protocolo é " + sheet.getRange(row,3).getValue() + ". Obrigado!!");
 }
